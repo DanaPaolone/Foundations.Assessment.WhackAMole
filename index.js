77226cc4edf97edb7129377d10dd94945eb37d14
DanaPaolone <dpaolone13@gmail.com>
2023-05-05 18:19:05 -0400

    almost working with timer and sound

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 //ACCESS #score, .hole
-const score = document.querySelector(".score");
+let scoreBoard = document.querySelector("span");
 const field = document.querySelector(".field");
 const holes = [...document.querySelectorAll(".hole")];
 //use spread operator to turn holes into an array
 //querySelectorAll - ALL was necessary to select multiple holes!
-
+let score = 0;
+const sound = new Audio("images/boing.mp3");
 //FUNCTIONS TO:
-//random mole in hole generator, timer
+//random mole in hole generator, timer, sound?
 function runGame() {
   //generate mole in random hole
-  //Math.random for 1-9
   let randomHole = Math.ceil(Math.random() * holes.length);
-  console.log(randomHole);
+  //console.log(randomHole);
   let hole = holes[randomHole];
-
+  let timer = "null";
   //create mole image from images file
   const mole = document.createElement("img");
   mole.className = "mole";
   mole.src = "images/mole.png";
-  //How do I append to a single hole???
-  // hole = holes[randomHole]
-  //field.removeChild(hole);
-  //hole.appendChild(mole);
   field.replaceChild(mole, hole);
+
+  //click even function includes reset hole, decrement timer, sound
+  mole.addEventListener("click", function () {
+    score += 10;
+    scoreBoard.textContent = score;
+    sound.play();
+    clearTimeout;
+    newTimer(timer);
+    field.replaceChild(hole, mole);
+    runGame();
+    2000 - 10;
+  });
+
+  //set timer to start
+  timer = setTimeout(() => {
+    field.replaceChild(hole, mole);
+    runGame();
+  }, 2000);
 }
 
 runGame();
