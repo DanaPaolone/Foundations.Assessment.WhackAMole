ede14be90379388cca9ade14ae0776247aa052f7
DanaPaolone <dpaolone13@gmail.com>
2023-05-05 20:01:47 -0400

    game is functiong

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 //ACCESS #score, .hole
 let scoreBoard = document.querySelector("span");
+const restartBtn = document.querySelector("#restart");
 const field = document.querySelector(".field");
 const holes = [...document.querySelectorAll(".hole")];
+const result = document.querySelector("p");
 //use spread operator to turn holes into an array
 //querySelectorAll - ALL was necessary to select multiple holes!
+
+let clickCount = 10;
 let score = 0;
 const sound = new Audio("images/boing.mp3");
+
 //FUNCTIONS TO:
 //random mole in hole generator, timer, sound?
 function runGame() {
   //generate mole in random hole
   let randomHole = Math.ceil(Math.random() * holes.length);
-  //console.log(randomHole);
   let hole = holes[randomHole];
+
   let timer = "null";
-  //create mole image from images file
+
   const mole = document.createElement("img");
   mole.className = "mole";
   mole.src = "images/mole.png";
@@ -22,27 +27,34 @@ function runGame() {
 
   //click even function includes reset hole, decrement timer, sound
   mole.addEventListener("click", function () {
-    score += 10;
-    scoreBoard.textContent = score;
     sound.play();
+
+    score += 1;
+    scoreBoard.textContent = score;
+
+    clickCount--;
+    if (clickCount === 1) {
+      result = `You finished with a score of ${score}`;
+    }
     clearTimeout;
     newTimer(timer);
     field.replaceChild(hole, mole);
     runGame();
-    2000 - 10;
+    500;
   });
 
   //set timer to start
   timer = setTimeout(() => {
     field.replaceChild(hole, mole);
     runGame();
-  }, 2000);
+  }, 1500);
 }
 
 runGame();
 
-//end game
+//end game - Ha!  It's ending at 5 clicks!  WHY???
 //restart game
-
-//EVENTLISTENER
-//click - run mole appears function, update score (and make sound?)
+restartBtn.addEventListener("click", function () {
+  score = 0;
+  runGame();
+});
